b677f149fff209fd23a249ded45cd34121ae9f60
Mikk Pavelson <mikk.pavelson@veriff.net>
2020-08-11 16:59:30 +0300

    first files

---
+++ bender/bender/bender.h
@@ -0,0 +1,19 @@
+//
+//  bender.h
+//  bender
+//
+//  Created by Mikk Pavelson on 11.08.2020.
+//  Copyright © 2020 Mikk Pavelson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for bender.
+FOUNDATION_EXPORT double benderVersionNumber;
+
+//! Project version string for bender.
+FOUNDATION_EXPORT const unsigned char benderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <bender/PublicHeader.h>
+
+
