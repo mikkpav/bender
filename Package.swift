160c86606c903b370e4d984f691f8d67886f537b
Mikk Pavelson <mikk.pavelson@veriff.net>
2020-08-12 11:35:55 +0300

    excluded objc files

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "bender",
-            dependencies: []),
+            dependencies: [],
+            exclude: ["Utils.m, Utils.h"]),
         .testTarget(
             name: "benderTests",
             dependencies: ["bender"]),
