7225716fdb7fd2d5ae534939a2b7c1bf13e5d9bd
Mikk Pavelson <mikk.pavelson@veriff.net>
2020-08-12 11:16:43 +0300

    added a util function

---
+++ Sources/bender/bender/BenderSmoother.swift
@@ -0,0 +1,15 @@
+//
+//  BenderSmoother.swift
+//  bender
+//
+//  Created by Mikk Pavelson on 12.08.2020.
+//  Copyright © 2020 Mikk Pavelson. All rights reserved.
+//
+
+import UIKit
+
+public class BenderSmoother {
+    func whatIsLife(name: String) -> String {
+        return "Life is \(name)"
+    }
+}
